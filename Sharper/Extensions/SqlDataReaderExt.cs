1885ee205d0ff84d30f134c319b2c6cda9a68a3b
gileswang <gileswang@gmail.com>
2017-05-15 19:46:05 +0800

    remove unused using

--- Sharper/Extensions/SqlDataReaderExt.cs
+++ Sharper/Extensions/SqlDataReaderExt.cs
@@ -3,9 +3,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Sharper.Extensions
